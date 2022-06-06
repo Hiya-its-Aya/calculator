c745ba2203b28ea0474e8b9fa965fdc96eb36323
ayamoosa123 <ayamoosa98@gmail.com>
2022-06-06 08:16:47 -0700

    make script

---
+++ script.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('button');
+const screen= document.querySelector('#calc-screen');
+
+
+
+//print buttons pressed on screen 
+// function numSelect(){
+    buttons.forEach(button =>{
+        button.addEventListener('click', function(){
+            console.log(className)
+            // const content = document.createElement('div');
+            // content.classList.add('content');
+            // content.textContent = "l";
+            // screen.appendchild(content)
+        })
+    })
+// }
+
+// numSelect()
+
+//clear, one click for current entry, two to start over
+//do math function 
+//return answer on screen 
+
